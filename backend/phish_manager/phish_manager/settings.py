ba689f62739db2abb3a02a4151dcec69d45c800a
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2020-08-22 12:40:32 -0400

    Fixed the improper import in settings.py
    - Migrated database

--- backend/phish_manager/phish_manager/settings.py
+++ backend/phish_manager/phish_manager/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'phisherman.apps.PhishermanConfig',
+    'phish_manager.phisherman',
 ]
 
 MIDDLEWARE = [
